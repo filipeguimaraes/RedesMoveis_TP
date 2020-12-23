4b747e80e9d47f612c2781369d7959ce099bf71a
Filipe Guimarães <fmtfg99@gmail.com>
2020-12-23 10:25:58 +0000

    Problema nos eventos resolvido

--- Ferramentas/src/EventsCreator.java
+++ Ferramentas/src/EventsCreator.java
@@ -11,14 +11,17 @@ public class EventsCreator {
             FileWriter myWriter = new FileWriter("events.txt");
             myWriter.write("Events.nrof = 100\n");
             myWriter.write("###################################\n");
+            int k = 0;
             for (int i = 0; i<=99; i++){
                 myWriter.write("Events"+(i+1)+".class = MessageEventGenerator\n");
                 myWriter.write("Events"+(i+1)+".interval = 60,60\n");
                 myWriter.write("Events"+(i+1)+".size = 10k,10k\n");
                 myWriter.write("Events"+(i+1)+".hosts = "+i+","+i+"\n");
                 myWriter.write("Events"+(i+1)+".tohosts = 100,105\n");
-                myWriter.write("Events"+(i+1)+".prefix = M\n");
+                myWriter.write("Events"+(i+1)+".prefix = M"+k+"\n");
+                myWriter.write("Events"+(i+1)+".time = 0, 600\n");
                 myWriter.write("###################################\n");
+                k++;
             }
             myWriter.close();
             System.out.println("Successfully wrote to the file.");
