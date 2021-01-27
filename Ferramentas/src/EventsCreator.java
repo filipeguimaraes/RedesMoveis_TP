f672b7fd8448407fe52e55a4e8fdd895f2da3da3
Filipe Guimarães <fmtfg99@gmail.com>
2021-01-27 09:54:11 +0000

    final

--- Ferramentas/src/EventsCreator.java
+++ Ferramentas/src/EventsCreator.java
@@ -1,7 +1,6 @@
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.Scanner;
 
 public class EventsCreator {
 
@@ -12,17 +11,18 @@ public class EventsCreator {
             myWriter.write("Events.nrof = 100\n");
             myWriter.write("###################################\n");
             int k = 0;
-            for (int i = 0; i<=99; i++){
-                myWriter.write("Events"+(i+1)+".class = MessageEventGenerator\n");
-                myWriter.write("Events"+(i+1)+".interval = 60,60\n");
-                myWriter.write("Events"+(i+1)+".size = 10k,10k\n");
-                myWriter.write("Events"+(i+1)+".hosts = "+i+","+i+"\n");
-                myWriter.write("Events"+(i+1)+".tohosts = 100,105\n");
-                myWriter.write("Events"+(i+1)+".prefix = M"+k+"\n");
-                myWriter.write("Events"+(i+1)+".time = 0, 600\n");
+            for (int i = 0; i <= 99; i++) {
+                myWriter.write("Events" + (i + 1) + ".class = MessageEventGenerator\n");
+                myWriter.write("Events" + (i + 1) + ".interval = 60,60\n");
+                myWriter.write("Events" + (i + 1) + ".size = 10k,10k\n");
+                myWriter.write("Events" + (i + 1) + ".hosts = " + i + "," + i + "\n");
+                myWriter.write("Events" + (i + 1) + ".tohosts = 100,105\n");
+                myWriter.write("Events" + (i + 1) + ".prefix = M" + k + "\n");
+                myWriter.write("Events" + (i + 1) + ".time = 0, 600\n");
                 myWriter.write("###################################\n");
                 k++;
             }
+
             myWriter.close();
             System.out.println("Successfully wrote to the file.");
         } catch (IOException e) {
@@ -32,7 +32,7 @@ public class EventsCreator {
 
     }
 
-    public static void create(String path){
+    public static void create(String path) {
         try {
             File myObj = new File(path);
             if (myObj.createNewFile()) {
